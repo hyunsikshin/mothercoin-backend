28257f82230650de68b002e057ff4b606654c05f
hyunsik <hyunsik.dev@gmail.com>
2019-01-30 11:50:53 +0900

    REST end point 추가

--- index.js
+++ index.js
@@ -2,6 +2,23 @@ import { GraphQLServer } from 'graphql-yoga';
 import { prisma } from './generated/prisma-client';
 import resolvers from './graphql/resolvers';
 
+// REST API REST API REST API REST API REST API  시작
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/user/domain/:domain', async (req, res) => {
+  const { domain } = req.params;
+  const post = await prisma.user({ domain });
+  res.json(post);
+});
+
+app.listen(3000, () => console.log('rest Server is running'));
+// REST API REST API REST API REST API REST API 끝
+
 const server = new GraphQLServer({
   typeDefs: './graphql/schema.graphql',
   resolvers,
